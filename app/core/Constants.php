22308323ee0cfb1231315a6c0f2bd8ed5fd753bb
Floydss <theanbou0101@gmail.com>
2021-11-10 17:14:29 +0700

    phpmvc

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost:81/phpmvc/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,8 @@
+<div class="container">
 
-    <h1>About Me</h1>
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/neko1.jpg" alt="Rahman" width="250" class="rounded-circle">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']?> tahun, saya adalah seorang <?= $data['pekerjaan']?>.</p>
+
+
+    </div>
